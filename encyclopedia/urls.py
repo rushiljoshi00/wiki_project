3b81ca95f9468b1942bb3d169a0b66d7f2e7afc1
rushiljoshi00 <rushil.joshi@ryerson.ca>
2022-07-14 21:35:12 -0400

    finished all features

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -4,6 +4,12 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    #we had to add wiki/ here because before in the search function
+    #when we tried to redirect from the home page by typing in the word css, it was conflicting with the /search
+    #and whatever we typed in, ex CSS. Before 
     path("wiki/<str:entry_content>", views.entry, name = "entry"),
-    path("search", views.search , name= "search")
+    path("search", views.search , name= "search"),
+    path("random", views.random_function , name= "random"),
+    path("create", views.create , name= "create"),
+    path("wiki/<str:entry>/edit", views.edit , name= "edit")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,9 +1,23 @@
+import random
 from django import forms
 from django.http import HttpResponseRedirect
 from django.shortcuts import redirect, render
 from django.urls import reverse
 from markdown2 import Markdown
 from . import util #from the current directory import util.py
+
+class CreateNewPage(forms.Form):
+    article_title = forms.CharField(label="Article Title", widget=forms.TextInput(attrs={
+        "placeholder": "Article Title",
+        "class" : "form-control col-md-8 col-lg-8",
+
+    }))
+    article_content = forms.CharField(label = "Article Content", widget=forms.Textarea(attrs={
+        "class" : "form-control col-md-8 col-lg-8"
+    
+    }))
+
+    editable = forms.BooleanField(initial=False,required=False,widget=forms.HiddenInput())
 
 
 def index(request):
@@ -23,32 +37,75 @@ def entry(request,entry_content):
         })
     else:
         return render(request,"encyclopedia/entry.html",{
-           "entry_content": markdown.convert(entry_title)   
+            "entry_content": markdown.convert(entry_title),
+            "entry_title" : entry_content
         })
 
 def search(request):
-    search_entry = request.GET.get('q','')
+    search_entry = request.GET.get("q","")
     if(util.get_entry(search_entry) is not None):
-        return HttpResponseRedirect(reverse("entry",kwargs={'entry_content':search_entry}))
+        return HttpResponseRedirect(reverse("entry",kwargs={"entry_content":search_entry}))
     else:
         wiki_substring = []
         for entry in util.list_entries():
             if search_entry.upper() in entry.upper():
                 wiki_substring.append(entry)
         
-        return render(request,"encyclopedia/index.html",{
+        return render(request,"encyclopedia/search.html",{
             "entries" : wiki_substring,
-            "search_entry" : search_entry,
-            "search_result" : True
+            "search_entry" : search_entry
         })
 
 
-def random(request):
-    None
+def random_function(request):
+    list_of_entries = util.list_entries()
+    chosen_list = random.choice(list_of_entries)
 
-def edit(request):
-    None
+    return HttpResponseRedirect(reverse("entry",kwargs={"entry_content":chosen_list}))
 
-def create(reqeust):
-    None 
 
+def create(request):
+
+    if request.method == "POST":
+        form = CreateNewPage(request.POST)
+        
+
+        if form.is_valid():
+            article_title = form.cleaned_data["article_title"]
+            article_content = form.cleaned_data["article_content"]
+        
+            
+            if util.get_entry(article_title) is None or form.cleaned_data["editable"] is True:
+                util.save_entry(article_title, article_content)
+                return HttpResponseRedirect(reverse("entry",kwargs={"entry_content":article_title}))
+            else:
+                return render(request,"encyclopedia/create.html",{
+                "form" : form,
+                "entry_title" : article_title,
+                "page_exists" : True
+            })            
+
+
+    return render(request, "encyclopedia/create.html", {
+        "form" : CreateNewPage()
+    }) 
+
+def edit(request,entry):
+    entry_content = util.get_entry(entry)
+    if entry_content is None:
+        return render(request,"encyclopedia/error.html",{
+            "entry_title" : entry
+        })
+
+    else:
+        form = CreateNewPage()
+        form.fields["article_title"].initial = entry
+        form.fields["article_title"].widget = forms.HiddenInput()
+        form.fields["article_content"].initial = entry_content
+        form.fields["editable"].initial = True
+       
+        return render(request,"encyclopedia/create.html",{
+            "form" : form,
+            "edit" : form.fields["editable"].initial,
+            "entry_title" : form.fields["article_title"].initial
+        })
